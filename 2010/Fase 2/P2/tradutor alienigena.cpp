cd7466855a45713039a05239d0cb132db58f808c
luanzito010 <140329902+luanzito010@users.noreply.github.com>
2024-06-12 15:35:00 -0400

    Update tradutor alienigena.cpp

--- 2010/Fase 2/P2/tradutor alienigena.cpp
+++ 2010/Fase 2/P2/tradutor alienigena.cpp
@@ -5,23 +5,17 @@ using namespace std;
 
 const int maxn = 1e5 + 5,maxp = 1e2 + 5,mod = 1e9 + 7;
 
-string x,y;
-int a[maxp],b[maxn],check[maxn],dp[maxn][maxp];
+string a,b;
+int check[maxn],dp[maxp][maxp];
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    cin >> x >> y;
+    cin >> a >> b;
     
-    int tam = sz(x);
-    int tam2 = sz(y);
-
-    for(int i = 0;i < tam;i++) 
-        a[i] = x[i] - '0';
-    
-    for(int i = 0;i < tam2;i++) 
-        b[i] = y[i] - '0';
+    int tam = sz(a);
+    int tam2 = sz(b);
 
     dp[0][0] = 1;
 
@@ -37,16 +31,22 @@ int main(){
             }
         }
         
+        bool ult = 0;
+        
         for(int j = 1;j <= min(tam,i);j++){
-            if(!b[i - j]) continue;
+            if(b[i - j] == '0') continue;
             if(j == tam && check[i - j]) break;
-            dp[i + 1][j + 1] = dp[i][j] % mod;
-            dp[i][0] = (dp[i][0] + dp[i][j]) % mod;
+            dp[(i + 1) % maxp][j + 1] = dp[i % maxp][j] % mod;
+            if(!ult){ 
+                dp[i % maxp][0] = dp[i % maxp][j] % mod;
+                ult = 1;
+            }
+            else dp[i % maxp][0] = (dp[i % maxp][0] + dp[i % maxp][j]) % mod;
         }
         
-        dp[i + 1][1] = dp[i][0] % mod;
+        dp[(i + 1) % maxp][1] = dp[i % maxp][0] % mod;
     }
 
-    cout << dp[tam2][0];
+    cout << dp[tam2 % maxp][0];
     return 0;
 }
